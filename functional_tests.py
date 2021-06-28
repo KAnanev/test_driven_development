01313062058cf40b5708ed796758487ac2ce3466
KAnanev <ananevdeveloper@yandex.ru>
2021-06-28 23:49:53 +0300

    Добавлено обработка POST запроса и передача переменных.

--- functional_tests.py
+++ functional_tests.py
@@ -24,11 +24,11 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text # 1 
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element_by_id('id_new_item') # 1
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -36,31 +36,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (её хобби -
         # вязание рыболовных мушек)
-        inputbox.send_keys('Купить павлиньи перья') # 2
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлинья перьи" в качестве элемента списка.
-        inputbox.send_keys(Keys.ENTER) # 3
-        time.sleep(1) # 4
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr') # 1
-        self.assertTrue(
-                any(row.text == '1: Купить павлиньи перья' for row in rows),
-                "Новый элемент списка не появился в таблице!"
-            )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        self.fail('Закончить тест')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        # Страница снова обнавляется и теперь показывает оба элемента ее списка
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт
         # сгенерировал для неё уникальный URL-адрес - об этом
         # выводится небольшой текст с объяснениями.
+        self.fail('Закончить тест')
 
         # Она посещает этот URL-адрес - ее список по-прежнему там.
 
