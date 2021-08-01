f203ae671d3eaf0d2e3a2f0ef15e353df9f0421f
KAnanev <ananevdeveloper@yandex.ru>
2021-08-01 13:57:28 +0300

    Применена get_absolute_url к модели списка для удаления дублирования url в представлениях (правило DRY)

--- lists/tests/test_model.py
+++ lists/tests/test_model.py
@@ -44,6 +44,9 @@ class ListAndItemModelTest(TestCase):
 			item.save()
 			item.full_clean()
 
-
+	def test_get_absolute_url(self):
+		"""Тест: Получен абсолютный URL."""
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
 
